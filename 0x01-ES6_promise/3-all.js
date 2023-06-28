890d441697a5f9d4072abc5d9a58db47db0da952
Mina Sami Anwar <62221411+mina-droid@users.noreply.github.com>
2023-06-28 04:39:37 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { createUser, uploadPhoto } from './utils.js';
+import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
-    return Promise.all([createUser(), uploadPhoto()])
-        .then((data) => {
-        console.log(`${data[1].body} ${data[0].firstName} ${data[0].lastName}`);
-    },)
-        .catch(() => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
